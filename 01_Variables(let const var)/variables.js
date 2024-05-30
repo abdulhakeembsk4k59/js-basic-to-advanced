7134da67f4767dd356fd94947013c128058944de
Abdul Hakeem Shaik <50977399+abdulhakeembsk4k59@users.noreply.github.com>
2024-05-30 05:22:48 +0000

    variables update

---
+++ 01_Variables(let const var)/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let email = "sk.a.hakeem123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+// accountId = 2  // not allowed because its constent
+
+email = "abc@gmail.com"
+accountPassword = "2323432"
+accountCity = "Manipur"
+
+console.table([accountId, email, accountPassword, accountCity])
+
